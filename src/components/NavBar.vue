c004f618519ab6fa86050509c255a46959c14a97
Vahe Sevachyan <vscode13@gmail.com>
2024-02-01 22:37:12 -0800

    navbar boiler plate

---
+++ src/components/NavBar.vue
@@ -0,0 +1,28 @@
+<script setup></script>
+
+<template>
+  <div class="nav-container">
+    <div>
+      <ul>
+        Product
+      </ul>
+      <ul>
+        Category
+      </ul>
+      <ul>
+        Contact
+      </ul>
+    </div>
+
+    <div>
+      <button>Login</button>
+      <button>Sign Up</button>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
