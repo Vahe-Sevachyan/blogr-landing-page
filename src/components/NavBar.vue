37e38be3d4e635ea54d8487ffe8ae6b47b97411f
Vahe Sevachyan <vscode13@gmail.com>
2024-02-02 19:22:46 -0800

    updated layout in nav section

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,24 +1,21 @@
 <script>
-// export default {
-//   name: "NavBar",
-// };
+export default {
+  name: "NavBar",
+};
 </script>
 
 <template>
-  <div class="nav-container">
-    <div>
-      <ul>
-        Product 1
-      </ul>
-      <ul>
-        Category
-      </ul>
-      <ul>
-        Contact
+  <div class="nav-section">
+    <div class="nav-menu">
+      <img src="../assets/images/logo.svg" alt="blog logo" />
+      <ul class="nav-links">
+        <li>Product</li>
+        <li>Category</li>
+        <li>Contact</li>
       </ul>
     </div>
 
-    <div>
+    <div class="btn-container">
       <button>Login</button>
       <button>Sign Up</button>
     </div>
@@ -26,10 +23,27 @@
 </template>
 
 <style scoped>
-.read-the-docs {
-  color: #888;
+.nav-section {
+  background-color: grey;
+  width: 90rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 }
-/* ul {
-  color: white;
-} */
+.nav-menu {
+  display: flex;
+  width: 23rem;
+  justify-content: space-between;
+}
+.nav-links {
+  display: flex;
+  width: 40rem;
+  justify-content: space-between;
+  list-style: none;
+}
+.btn-container {
+  width: 10rem;
+  display: flex;
+  justify-content: space-between;
+}
 </style>
